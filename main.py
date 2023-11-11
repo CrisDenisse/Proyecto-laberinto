add36e337171da43054f31903a1d5352e9934630
Denisse Arteaga <149846945+CrisDenisse@users.noreply.github.com>
2023-11-10 22:26:59 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+# main.py
+
+# Pedir el nombre del jugador por teclado
+nombre_jugador = input("Ingrese su nombre: ")
+
+# Imprimir un mensaje de bienvenida con el nombre
+print(f"Bienvenido, {nombre_jugador}!")
